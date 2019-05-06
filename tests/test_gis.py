a04a1441e0a7a47facd8d8807f775ee234ed1b48
Erkka Rinne <erkka.rinne@vtt.fi>
2019-05-06 15:54:01 +0300

    Formatting edits

--- tests/test_gis.py
+++ tests/test_gis.py
@@ -1,3 +1,6 @@
+"""Test GIS functionality"""
+# pylint: disable=missing-docstring
+
 import os.path
 
 from numpy import isclose
@@ -7,6 +10,6 @@ from metreload.gis import get_shapefile_bbox
 
 def test_bbox(datadir):
     assert isclose(get_shapefile_bbox(os.path.join(datadir, 'areas.shp')),
-                   ( 2.225849462962977, -7.784538191975432,
-                    -9.59606517346118,  13.428893993210842)).all()
+                   (2.225849462962977, -7.784538191975432,
+                    -9.59606517346118, 13.428893993210842)).all()
     
