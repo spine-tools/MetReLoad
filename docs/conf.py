07f68387769d3b1367ebd561a07a3f2c152391b0
Erkka Rinne <erkka.rinne@vtt.fi>
2019-08-23 11:41:47 +0300

    Use Sphinx AutoAPI for API documentation

--- docs/conf.py
+++ docs/conf.py
@@ -45,7 +45,8 @@ release = version
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.coverage',
-    'sphinx.ext.napoleon'
+    'sphinx.ext.napoleon',
+    'autoapi.extension',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -75,6 +76,10 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
 
+# Settings for Sphinx AutoAPI
+autoapi_dirs = ['../metreload']  # packag[e to be documented
+autoapi_ignore = ['*/spec.py']  # ignored modules
+
 
 # -- Options for HTML output -------------------------------------------------
 
@@ -92,7 +97,7 @@ html_theme = 'alabaster'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = []
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
